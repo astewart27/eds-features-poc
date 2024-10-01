039a06382dbc436dd4fd22218cf6b07630fdf3a4
Aaron Stewart <aaron.stewart@cambriausa.com>
2024-10-01 10:38:40 -0700

    Updated link

--- blocks/link/link.js
+++ blocks/link/link.js
@@ -1,3 +1,15 @@
 export default function decorate(block) {
-  console.log('Link Block', block);
+  [...block.children].forEach((row, index) => {
+    if (index === 1) {
+      const anchor = block.querySelector('a');
+      const newTabEl = row.querySelector('p');
+      if (newTabEl) {
+        const target = newTabEl.innerText === 'false' ? '_self' : '_blank';
+        anchor?.setAttribute('target', target);
+      } else {
+        anchor?.setAttribute('target', '_self');
+      }
+      row.remove();
+    }
+  });
 }
